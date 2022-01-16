f7f87313ca6c2678f03a003b348734806b65a7a2
Nathan <nathanrobertmarshall@gmail.com>
2022-01-16 15:03:43 -0600

    config file and ping message

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # file-formats-discord-bot
 A proof of concept discord bot for The File Formats server.
 
-# Resources:
+# Set up
+Add a config.json file just like config.json.sample.
+
+Specify the bot's token there.
+
+# Resources
 https://youtu.be/j_sD9udZnCk
+
+https://discordapi.com/permissions.html
--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const { Client, Intents } = require('discord.js');
 
+var config = require('./config.json');
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const prefix = "-"
 
 client.once('ready', () => {
     console.log('The File Formats bot is online!');
 });
 
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+});
+
 // TODO Need local config file to store token. Token below is not used anymore by discord.
-client.login('ODA3MDY0MjYyNTA4OTM3MjU3.YByjKg.-Sww0W7thWlyXguaMK423I0amhI');
+client.login(config.token);
