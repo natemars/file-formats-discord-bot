e0c567606754ebf4d1568c52b8e9b0301e3bbde5
Nathan <nathanrobertmarshall@gmail.com>
2022-01-16 14:26:54 -0600

    minor edits

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # file-formats-discord-bot
 A proof of concept discord bot for The File Formats server.
+
+# Resources:
+https://youtu.be/j_sD9udZnCk
--- main.js
+++ main.js
@@ -6,4 +6,5 @@ client.once('ready', () => {
     console.log('The File Formats bot is online!');
 });
 
+// TODO Need local config file to store token. Token below is not used anymore by discord.
 client.login('ODA3MDY0MjYyNTA4OTM3MjU3.YByjKg.-Sww0W7thWlyXguaMK423I0amhI');
