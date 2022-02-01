7b868049c16617de638a5c57ef23ea1ae07e5c3b
Nathan <nathanrobertmarshall@gmail.com>
2022-01-31 19:59:28 -0600

    more work for today

--- index.ts
+++ index.ts
@@ -14,16 +14,44 @@ client.on('ready', () => {
     console.log('The bot is ready!')
     const fileFormatsGuildId = '322254386443190273'
     const guild = client.guilds.cache.get(fileFormatsGuildId)
-  })
-  // Listen for new messages
-  client.on('messageCreate', (msg) => {
-    // Was the message "ping"?
-    if (msg.content === 'ping') {
-      // Reply with "pong"
-      msg.reply({
-        content: 'pong',
-      })
+    let commands
+    if(guild){
+      commands = guild.commands
     }
+    else{
+      commands = client.application?.commands
+    }
+
+    commands?.create({
+      name: 'ping',
+      description: 'Replies with pong',
+    })
   })
+
+client.on('interactionCreate', async (interation) => {
+  if(!interation.isCommand()){
+    return
+  }
+
+  const { commandName, options } = interation
+
+  if(commandName == 'ping'){
+    interation.reply({
+      content: 'pong',
+      ephemeral: true,
+    })
+  }
+})
+
+  // Listen for new messages
+  // client.on('messageCreate', (msg) => {
+  //   // Was the message "ping"?
+  //   if (msg.content === 'ping') {
+  //     // Reply with "pong"
+  //     msg.reply({
+  //       content: 'pong',
+  //     })
+  //   }
+  // })
 
   client.login(process.env.TOKEN)
